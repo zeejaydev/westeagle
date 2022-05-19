5688acb1c26b6d529e2f836f256a5670f75f0375
Zaid Jamil <zeejaydev@gmail.com>
2022-05-19 02:09:07 -0600

    test

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ app.use(express.json());
 app.use(express.static('./public'));
 app.use(express.static('./public/css'));
 app.use(express.static('./public/js'));
+app.use(express.static('./public/assets'));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    // res.sendFile(path.join(__dirname, './public/home.html'));
+    res.sendFile(path.join(__dirname, './public/home.html'));
 });
 const port = process.env.PORT || 3000
 app.listen(port,()=>console.log('server running'))
